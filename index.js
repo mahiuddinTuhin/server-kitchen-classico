fb7dfc5dca2b68b940eafffce5dea2100c891c11
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2022-11-10 07:18:17 +0600

    mongodb added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_password}@cluster0.yfdgs6q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("server is active");
+  console.log(uri);
+});
+
+app.listen(port, () => {
+  console.log(`server working on port: ${port}`);
+});
